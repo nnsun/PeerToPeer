e2bc04c3f88df2069026c4271fcfb3cd7ea8e67e
Ning Ning Sun <nsun200@live.com>
2017-10-29 22:29:16 -0400

    Work on advertising/discovering

--- app/src/main/java/com/example/peertopeer/PeersListActivity.java
+++ app/src/main/java/com/example/peertopeer/PeersListActivity.java
@@ -1,17 +1,19 @@
 package com.example.peertopeer;
 
+import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
-import android.content.Context;
+import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.net.wifi.p2p.WifiP2pManager.Channel;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
+import android.text.Html;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -27,31 +29,40 @@ import com.google.android.gms.nearby.connection.DiscoveryOptions;
 import com.google.android.gms.nearby.connection.EndpointDiscoveryCallback;
 import com.google.android.gms.nearby.connection.Strategy;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.TreeMap;
 
-public class PeersListActivity extends SingleFragmentActivity implements GoogleApiClient.ConnectionCallbacks,
+
+public class PeersListActivity extends Activity implements GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener {
 
     private WifiP2pManager mManager;
     private Channel mChannel;
-    private ServiceBase mWifiDirectReceiver;
 
     private BluetoothAdapter mBluetoothAdapter;
     private final static int REQUEST_ENABLE_BT = 1;
 
-    private PeersListFragment mFragment;
-
     public GoogleApiClient mGoogleApiClient;
 
+    private ServiceBase mServiceBase;
+
+    private BluetoothListener mBluetoothListener;
+    private String mDeviceName;
+
+    private GossipData mData;
+    public static List<String> mBluetoothDevices;
+
+    private final String[] mColors = { "BLACK", "BLUE", "CYAN", "GREEN", "MAGENTA", "RED", "YELLOW" };
+    private HashMap<String, String> mColorMap;
+
     @Override
-    public Fragment createFragment() {
-        mManager = (WifiP2pManager)getSystemService(Context.WIFI_P2P_SERVICE);
-        mChannel = mManager.initialize(this, getMainLooper(), null);
-        mWifiDirectReceiver = new ServiceBase(mManager, mChannel, this);
-
-        mFragment = new PeersListFragment();
-
-        mGoogleApiClient = new GoogleApiClient.Builder(this).addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this).addApi(Nearby.CONNECTIONS_API).build();
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if (mBluetoothAdapter != null) {
@@ -60,114 +71,180 @@ public class PeersListActivity extends SingleFragmentActivity implements GoogleA
                 Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
                 startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
             }
-
-        }
-        else {
-            Log.e("p2p_log", "Device does not support Bluetooth");
         }
 
-        mFragment.setBluetoothArgs(mBluetoothAdapter);
-        mFragment.setWifiDirectArgs(mManager, mChannel, mWifiDirectReceiver);
-        return mFragment;
-    }
+        mDeviceName = mBluetoothAdapter.getName();
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        mBluetoothListener = new BluetoothListener(mBluetoothAdapter);
+
         mGoogleApiClient = new GoogleApiClient.Builder(this).addConnectionCallbacks(this).
                 addOnConnectionFailedListener(this).addApi(Nearby.CONNECTIONS_API).build();
-    }
 
+        mGoogleApiClient.connect();
 
-    private final EndpointDiscoveryCallback mEndpointDiscoveryCallback =
-            new EndpointDiscoveryCallback() {
-                @Override
-                public void onEndpointFound(
-                        String endpointId, DiscoveredEndpointInfo discoveredEndpointInfo) {
-                    // An endpoint was found!
-//                    mBluetoothDevices.add(endpointId);
+        mServiceBase = new ServiceBase(this);
 
-                    String name = android.os.Build.MODEL;
-                    Nearby.Connections.requestConnection(mGoogleApiClient, name, endpointId, new ConnectionLifecycleCallback() {
-                        @Override
-                        public void onConnectionInitiated(
-                                String endpointId, ConnectionInfo connectionInfo) {
-                            // Automatically accept the connection on both sides.
-                            Nearby.Connections.acceptConnection(
-                                    mGoogleApiClient, endpointId, null);
+        setContentView(R.layout.main_activity);
+
+        mColorMap = new HashMap<>();
+        mBluetoothDevices = new ArrayList<>();
+
+        mBluetoothListener.mBluetoothServer.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+
+        Log.d("p2p_log", "Is connecting: " + mGoogleApiClient.isConnecting());
+
+        AsyncTask timer = new AsyncTask() {
+            @Override
+            protected Object doInBackground(Object[] objects) {
+                while (true) {
+                    try {
+                        Thread.sleep(10000);
+
+                        if (mData == null) {
+                            mData = GossipData.get(mDeviceName);
                         }
 
-                        @Override
-                        public void onConnectionResult(String endpointId, ConnectionResolution result) {
-                            switch (result.getStatus().getStatusCode()) {
-                                case ConnectionsStatusCodes.STATUS_OK:
-                                    // We're connected! Can now start sending and receiving data.
-                                    Log.d("p2p_log", "Connection successful!");
-                                    break;
-                                case ConnectionsStatusCodes.STATUS_CONNECTION_REJECTED:
-                                    // The connection was rejected by one or both sides.
-                                    Log.d("p2p_log", "Connection failed");
-                                    break;
+                        Random random = new Random();
+                        if (random.nextBoolean()) {
+                            mData.add(random.nextInt(100) + 1, mDeviceName);
+                        }
+
+
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                updateUI();
                             }
-                        }
+                        });
 
-                        @Override
-                        public void onDisconnected(String endpointId) {
-                            // We've been disconnected from this endpoint. No more data can be
-                            // sent or received.
-                            Log.d("p2p_log", "Disconnected from endpoint");
-                        }
-                    }).setResultCallback(
-                            new ResultCallback<Status>() {
-                                @Override
-                                public void onResult(@NonNull Status status) {
-                                    if (status.isSuccess()) {
-                                        // We successfully requested a connection. Now both sides
-                                        // must accept before the connection is established.
-                                    }
-                                    else {
-                                        // Nearby Connections failed to request the connection.
-                                        Log.d("p2p_log", "Connection request failed");
-                                    }
+                        int numPeers = mBluetoothDevices.size();
+
+                        if (numPeers > 0) {
+                            int randomPeer = random.nextInt(numPeers);
+
+                            String peer = mBluetoothDevices.get(randomPeer);
+
+                            Log.d("p2p_log", "Trying to connect to " + peer);
+
+                            BluetoothDevice device = mBluetoothAdapter.getRemoteDevice("E4:58:B8:E0:6D:15");
+
+                            BluetoothSocket socket = null;
+
+                            long start = System.currentTimeMillis();
+                            try {
+                                // Get a BluetoothSocket to connect with the given BluetoothDevice.
+                                // MY_UUID is the app's UUID string, also used in the server code.
+                                socket = device.createInsecureRfcommSocketToServiceRecord(BluetoothListener.MY_UUID);
+
+                            }
+                            catch (IOException e) {
+                                Log.e("p2p_log", "Socket's create() method failed", e);
+                            }
+
+                            try {
+                                // Connect to the remote device through the socket. This call blocks
+                                // until it succeeds or throws an exception.
+                                socket.connect();
+                                Log.d("p2p_log", "Successfully connected in " + (System.currentTimeMillis() - start) + " milliseconds");
+
+                                FileOperations.bluetoothSendData(socket, mDeviceName);
+//                                Log.d("p2p_log", "Successfully sent in " + (System.currentTimeMillis() - start) + " milliseconds");
+
+                                FileOperations.bluetoothGetData(socket, mDeviceName);
+//                                Log.d("p2p_log", "Successfully received in " + (System.currentTimeMillis() - start) + " milliseconds");
+                            }
+                            catch (IOException connectException) {
+                                // Unable to connect
+                                Log.d("p2p_log", "Bluetooth socket connect failed");
+                                try {
+                                    socket.close();
                                 }
-                            });
-                }
+                                catch (IOException e) {
+                                    Log.d("p2p_log", "Failed to close Bluetooth socket");
+                                }
+                            }
 
-                @Override
-                public void onEndpointLost(String endpointId) {
-                    // A previously discovered endpoint has gone away.
-//                    mBluetoothDevices.remove(endpointId);
-                }
-            };
-
-
-    private void startDiscovery() {
-        Log.d("p2p_log", "Discovering services");
-
-        Nearby.Connections.startDiscovery(mGoogleApiClient, "",
-                mEndpointDiscoveryCallback, new DiscoveryOptions(Strategy.P2P_CLUSTER)).setResultCallback(
-                new ResultCallback<Status>() {
-                    @Override
-                    public void onResult(@NonNull Status status) {
-                        if (status.isSuccess()) {
-                            // We're discovering!
-                            Log.d("p2p_log", "Started discovery");
-                        }
-                        else {
-                            // We were unable to start discovering.
-                            Log.d("p2p_log", "Start discovery failed");
                         }
                     }
-                });
+                    catch (InterruptedException e) {
+                        Log.e("p2p_log", e.toString());
+                        break;
+                    }
+                }
+                return null;
+            }
+        };
 
-
+        timer.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
     }
 
+
+    public final EndpointDiscoveryCallback mEndpointDiscoveryCallback = new EndpointDiscoveryCallback() {
+        @Override
+        public void onEndpointFound(String endpointId, DiscoveredEndpointInfo discoveredEndpointInfo) {
+            // An endpoint was found!
+            Log.d("p2p_log", "Endpoint found: " + endpointId);
+            mBluetoothDevices.add(endpointId);
+
+            String name = android.os.Build.MODEL;
+            Nearby.Connections.requestConnection(mGoogleApiClient, name, endpointId, new ConnectionLifecycleCallback() {
+                @Override
+                public void onConnectionInitiated(
+                        String endpointId, ConnectionInfo connectionInfo) {
+                    // Automatically accept the connection on both sides.
+                    Nearby.Connections.acceptConnection(
+                            mGoogleApiClient, endpointId, null);
+                }
+
+                @Override
+                public void onConnectionResult(String endpointId, ConnectionResolution result) {
+                    switch (result.getStatus().getStatusCode()) {
+                        case ConnectionsStatusCodes.STATUS_OK:
+                            // We're connected! Can now start sending and receiving data.
+                            Log.d("p2p_log", "Connection successful!");
+                            break;
+                        case ConnectionsStatusCodes.STATUS_CONNECTION_REJECTED:
+                            // The connection was rejected by one or both sides.
+                            Log.d("p2p_log", "Connection failed");
+                            break;
+                    }
+                }
+
+                @Override
+                public void onDisconnected(String endpointId) {
+                    // We've been disconnected from this endpoint. No more data can be
+                    // sent or received.
+                    Log.d("p2p_log", "Disconnected from endpoint");
+                }
+            }).setResultCallback(new ResultCallback<Status>() {
+                @Override
+                public void onResult(@NonNull Status status) {
+                    if (status.isSuccess()) {
+                        // We successfully requested a connection. Now both sides
+                        // must accept before the connection is established.
+                    }
+                    else {
+                        // Nearby Connections failed to request the connection.
+                        Log.d("p2p_log", "Connection request failed");
+                    }
+                }
+            });
+        }
+
+        @Override
+        public void onEndpointLost(String endpointId) {
+            // A previously discovered endpoint has gone away.
+//                    mBluetoothDevices.remove(endpointId);
+        }
+    };
 
     @Override
     public void onStart() {
         super.onStart();
-        mGoogleApiClient.connect();
+        Log.d("p2p_log", "On start");
+        if (!mGoogleApiClient.isConnected() && !mGoogleApiClient.isConnecting()) {
+            mGoogleApiClient.connect();
+        }
     }
 
     @Override
@@ -180,20 +257,40 @@ public class PeersListActivity extends SingleFragmentActivity implements GoogleA
 
     @Override
     public void onConnected(@Nullable Bundle bundle) {
+        Log.d("p2p_log", "GoogleApiClient connected");
 
+        mServiceBase.startAdvertising(mGoogleApiClient, mDeviceName);
+        mServiceBase.startDiscovery(mGoogleApiClient, mDeviceName);
     }
 
     @Override
     public void onConnectionSuspended(int i) {
-
+        Log.d("p2p_log", "Connection suspended");
     }
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
+        Log.d("p2p_log", "Connection failed");
     }
 
+    public void updateUI() {
+        if (mData == null) {
+            return;
+        }
 
+        TextView dataView = findViewById(R.id.data_view);
+        TreeMap<Integer, String> dataset = mData.getData();
 
+        String text = "Number of elements: " + dataset.size() + "<br />";
 
+        for (Map.Entry<Integer, String> entrySet : dataset.entrySet()) {
+            int num = entrySet.getKey();
+            String name = entrySet.getValue();
+            if (!mColorMap.containsKey(name)) {
+                mColorMap.put(name, mColors[mColorMap.size() % mColors.length]);
+            }
+            text += "<font color=" + mColorMap.get(name) + ">" + num + "</font>\t";
+        }
+        dataView.setText(Html.fromHtml(text), TextView.BufferType.SPANNABLE);
+    }
 }
--- app/src/main/java/com/example/peertopeer/ServiceBase.java
+++ app/src/main/java/com/example/peertopeer/ServiceBase.java
@@ -1,6 +1,5 @@
 package com.example.peertopeer;
 
-import android.net.wifi.p2p.WifiP2pManager;
 import android.net.wifi.p2p.WifiP2pManager.Channel;
 import android.net.wifi.p2p.nsd.WifiP2pDnsSdServiceInfo;
 import android.support.annotation.NonNull;
@@ -9,6 +8,7 @@ import android.util.Log;
 import com.google.android.gms.common.api.GoogleApi;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.nearby.Nearby;
 import com.google.android.gms.nearby.connection.AdvertisingOptions;
 import com.google.android.gms.nearby.connection.ConnectionInfo;
@@ -16,6 +16,9 @@ import com.google.android.gms.nearby.connection.ConnectionLifecycleCallback;
 import com.google.android.gms.nearby.connection.ConnectionResolution;
 import com.google.android.gms.nearby.connection.Connections;
 import com.google.android.gms.nearby.connection.ConnectionsStatusCodes;
+import com.google.android.gms.nearby.connection.DiscoveredEndpointInfo;
+import com.google.android.gms.nearby.connection.DiscoveryOptions;
+import com.google.android.gms.nearby.connection.EndpointDiscoveryCallback;
 import com.google.android.gms.nearby.connection.Strategy;
 
 import java.util.HashMap;
@@ -23,35 +26,53 @@ import java.util.HashMap;
 public class ServiceBase extends ConnectionLifecycleCallback {
     public static final String SERVICE_TYPE = "_P2P_demo._tcp";
 
-    private WifiP2pManager mManager;
     private Channel mChannel;
     private PeersListActivity mActivity;
 
-    public ServiceBase(WifiP2pManager manager, Channel channel, PeersListActivity activity) {
+    public ServiceBase(PeersListActivity activity) {
         super();
-
-        mManager = manager;
-        mChannel = channel;
         mActivity = activity;
     }
 
-    public void advertiseService(GoogleApiClient client, String address) {
-        Log.d("p2p_log", "Bluetooth MAC address: " + address);
-        Nearby.Connections.startAdvertising(client, address, address, this,
+    public void startAdvertising(GoogleApiClient client, String name) {
+        Log.d("p2p_log", "Device name: " + name);
+        Nearby.Connections.startAdvertising(client, name, name, this,
                     new AdvertisingOptions(Strategy.P2P_CLUSTER)).setResultCallback(
             new ResultCallback<Connections.StartAdvertisingResult>() {
                 @Override
                 public void onResult(@NonNull Connections.StartAdvertisingResult result) {
                     if (result.getStatus().isSuccess()) {
                         // We're advertising!
+                        Log.d("p2p_log", "Now advertising");
                     }
                     else {
                         // We were unable to start advertising.
+                        Log.d("p2p_log", "Failed to advertise");
                     }
                 }
             });
-
     }
+
+    public void startDiscovery(GoogleApiClient client, String name) {
+        Log.d("p2p_log", "Discovering services");
+
+        Nearby.Connections.startDiscovery(client, name, mActivity.mEndpointDiscoveryCallback, new DiscoveryOptions(Strategy.P2P_CLUSTER)).
+                setResultCallback(
+                new ResultCallback<Status>() {
+                    @Override
+                    public void onResult(@NonNull Status status) {
+                        if (status.isSuccess()) {
+                            // We're discovering!
+                            Log.d("p2p_log", "Started discovery");
+                        }
+                        else {
+                            // We were unable to start discovering.
+                            Log.d("p2p_log", "Start discovery failed");
+                        }
+                    }
+                });
+    }
+
 
     @Override
     public void onConnectionInitiated(String endpointId, ConnectionInfo connectionInfo) {
